eea918b281257943e253ace5bbce9569bc4705d4
leon.shi <superRaytin@163.com>
2016-01-14 22:04:33 +0800

    use var in strict mode

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+var fs = require('fs');
+var path = require('path');
 
 module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, '.eslintrc')));
--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const should = require('should');
-const eslintrc = require('../index');
+var should = require('should');
+var eslintrc = require('../index');
 
 describe('eslint-config-leonshi', function() {
   it('works', function() {
