64eca14c79b3b5219f572ae05820805c134ec064
leon.shi <superRaytin@163.com>
2016-01-14 22:09:24 +0800

    update test case

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,7 @@
 var should = require('should');
 var eslintrc = require('../index');
 
-describe('eslint-config-leonshi', function() {
+describe('eslint-config-leon', function() {
   it('works', function() {
     eslintrc.parser.should.equal('espree');
     eslintrc.parserOptions.ecmaVersion.should.equal(6);
